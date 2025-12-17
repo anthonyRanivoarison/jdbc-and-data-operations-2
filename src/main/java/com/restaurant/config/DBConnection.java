6e5553ced59081e12d323cd5a0c7eb0c0e3a8abd
anthonyRanivoarison <antonitsiory@gmail.com>
2025-12-17 14:52:59 +0300

    feat: create db connection

---
+++ src/main/java/com/restaurant/config/DBConnection.java
@@ -0,0 +1,34 @@
+package com.restaurant.config;
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    public static Connection getDBConnection() {
+        var dotenv = Dotenv.load();
+
+        String url = dotenv.get("JDBC_URL");
+        String username = dotenv.get("DB_USERNAME");
+        String password = dotenv.get("DB_PASSWORD");
+
+        try {
+            return DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void closeConnection() {
+        try {
+            var connection = getDBConnection();
+
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
